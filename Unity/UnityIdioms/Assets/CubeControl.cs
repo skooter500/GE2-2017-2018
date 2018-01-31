c202317a489711ec4337709a21b42f67aaa5f4b5
Bryan Duggan <skooter500@gmail.com>
2018-01-31 10:46:54 +0000

    Bla

---
+++ Unity/UnityIdioms/Assets/CubeControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour {
+
+    public float rotationSpeed = 180f;
+    public float moveSpeed = 20.0f;
+
+    public GameObject bulletSpawnPoint;
+    public GameObject bulletPrefab;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, - moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, - rotationSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
+            bullet.transform.position = bulletSpawnPoint.transform.position;
+            bullet.transform.rotation = transform.rotation;
+
+        }
+    }
+}
