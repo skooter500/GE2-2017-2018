4892c361f26af651189dbd939d06b46bfdef9a04
skooter500 <skooter500@gmail.com>
2019-01-29 15:19:13 +0000

    bla

---
+++ Unity/GE2Examples/Assets/CharacterMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMover : MonoBehaviour {
+
+    public GameObject target;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
