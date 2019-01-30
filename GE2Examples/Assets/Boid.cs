41b75f9a95fe8cf97eb5698db929a44b871f7d86
skooter500 <skooter500@gmail.com>
2019-01-30 11:56:59 +0000

    seek and arrive

---
+++ GE2Examples/Assets/Boid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass = 1;
+    public Vector3 target;
+    public float maxSpeed = 5;
+    public GameObject targetGameObject;
+    public float slowingDistance = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Vector3 Seek(Vector3 target)
+    {
+        Vector3 desired = target - transform.position;
+        desired.Normalize();
+        desired *= maxSpeed;
+        return desired - velocity;
+    }
+
+    public Vector3 Arrive(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        float dist = toTarget.magnitude;
+
+        float ramped = (dist / slowingDistance) * maxSpeed;
+        float clamped = Mathf.Min(ramped, maxSpeed);
+        Vector3 desired = clamped * (toTarget / dist);
+        return desired - velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetGameObject != null)
+        {
+            target = targetGameObject.transform.position;
+        }
+        force = Arrive(target);
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+        if (velocity.magnitude > float.Epsilon)
+        {
+            transform.forward = velocity;
+        }
+    }
+}
